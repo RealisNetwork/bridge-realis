0d5af08a36a8964bc08af5c473251e0c2e49409a
Daelon02 <Daelon02@github.com>
2021-07-26 11:29:43 +0300

    Delete bridge in bridge

--- bsc-sender/src/lib.rs
+++ bsc-sender/src/lib.rs
@@ -1,40 +1,39 @@
-use sp_core::H160;
-use web3::contract::Contract;
-use web3::types::{Address, U256};
-use web3::transports::WebSocket;
-use std::str::FromStr;
-use secp256k1::SecretKey;
-use runtime::realis_bridge;
-use realis_bridge::TokenId;
-use std::path::Path;
-use realis_adapter::BridgeEvents;
-use std::fs;
 use async_trait::async_trait;
 use logger::logger::{log, Type};
+use realis_adapter::BridgeEvents;
+use realis_bridge::TokenId;
+use runtime::realis_bridge;
+use secp256k1::SecretKey;
+use sp_core::H160;
+use std::fs;
+use std::path::Path;
+use std::str::FromStr;
+use web3::contract::Contract;
+use web3::transports::WebSocket;
+use web3::types::{Address, U256};
 
 pub struct BscSender {
     // web3: web3::Web3<WebSocket>,
     contract: Contract<WebSocket>,
-    wallet_key: SecretKey
+    wallet_key: SecretKey,
 }
 
 impl BscSender {
-
     pub async fn new(url: &str) -> BscSender {
         let wss = WebSocket::new(url).await.unwrap();
         let web3 = web3::Web3::new(wss);
 
-        let address: Address = Address::from_str("0x0db8499bb62772e805af78fc918ee8c8cd6a2859").unwrap();
+        let address: Address =
+            Address::from_str("0x0db8499bb62772e805af78fc918ee8c8cd6a2859").unwrap();
         let json_abi = include_bytes!("../res/BEP20.abi");
         let contract = Contract::from_json(web3.eth(), address, json_abi).unwrap();
-
 
         let wallet_key = BscSender::read_file_for_secret_key("./bsc-sender/res/accounts.key");
 
         BscSender {
             // web3,
             contract,
-            wallet_key
+            wallet_key,
         }
     }
 
@@ -51,13 +50,24 @@ impl BridgeEvents for BscSender {
         let to: Address = Address::from(to.0);
         let value = U256::from(*value) * 100_000_000;
 
-        let result = self.contract
-            .signed_call_with_confirmations("transfer", (to, value), Default::default(), 1, &self.wallet_key)
+        let result = self
+            .contract
+            .signed_call_with_confirmations(
+                "transfer",
+                (to, value),
+                Default::default(),
+                1,
+                &self.wallet_key,
+            )
             .await;
 
         match result {
-            Ok(value) => log(Type::Success, String::from("Transaction hash"), &value.transaction_hash),
-            Err(err) => log(Type::Error, String::from("Transaction fail"), &err)
+            Ok(value) => log(
+                Type::Success,
+                String::from("Transaction hash"),
+                &value.transaction_hash,
+            ),
+            Err(err) => log(Type::Error, String::from("Transaction fail"), &err),
         }
     }
 
@@ -68,13 +78,24 @@ impl BridgeEvents for BscSender {
         println!("Account BSC: {:?}", to);
         println!("Value: {:?}", value);
 
-        let result = self.contract
-            .signed_call_with_confirmations("transfer", (to, value), Default::default(), 1, &self.wallet_key)
+        let result = self
+            .contract
+            .signed_call_with_confirmations(
+                "transfer",
+                (to, value),
+                Default::default(),
+                1,
+                &self.wallet_key,
+            )
             .await;
 
         match result {
-            Ok(value) => log(Type::Success, String::from("Transaction hash"), &value.transaction_hash),
-            Err(err) => log(Type::Error, String::from("Transaction fail"), &err)
+            Ok(value) => log(
+                Type::Success,
+                String::from("Transaction hash"),
+                &value.transaction_hash,
+            ),
+            Err(err) => log(Type::Error, String::from("Transaction fail"), &err),
         }
     }
 }
