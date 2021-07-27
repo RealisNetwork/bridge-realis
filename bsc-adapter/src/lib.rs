0ea9856a76142b5ca8b7e7942601a93f91d53610
HolyGrease <kireevdanil2000@gmail.com>
2021-07-27 13:20:43 +0300

    refactor: remove unused staff

--- bsc-adapter/src/lib.rs
+++ bsc-adapter/src/lib.rs
@@ -16,68 +16,6 @@ use runtime::AccountId;
 use async_trait::async_trait;
 use std::convert::TryFrom;
 
-// #[tokio::main]
-// async fn main() {
-//
-//     // let url = "wss://bsc-ws-node.nariox.org:443";
-//     let url = "wss://data-seed-prebsc-1-s1.binance.org:8545/";
-//     let mut wss = WebSocket::new(url).await;
-//     loop {
-//         match wss {
-//             Ok(_) => break,
-//             Err(error) => {
-//                 log(Type::Error, String::from("Cannot connect"), &error);
-//                 log(Type::Info, String::from("Try to reconnect"), &());
-//                 wss = WebSocket::new(url).await;
-//             }
-//         }
-//     }
-//     let web3 = web3::Web3::new(wss.unwrap());
-//     let json_abi = include_bytes!("../../bsc-sender/res/BEP20.abi");
-//     let address: web3::types::H160 = web3::types::H160::from_str("0x987893D34052C07F5959d7e200E9e10fdAf544Ef").unwrap();
-//     let contract = Contract::from_json(web3.eth(), address, json_abi).unwrap();
-//     let from: Address = Address::from_str("0x6D1eee1CFeEAb71A4d7Fcc73f0EF67A9CA2cD943").unwrap();
-//
-//     loop {
-//
-//         let result: web3::contract::Result<Vec<(Address, Bytes, Uint)>> = contract
-//             .events(
-//                 "TransferToRealis",
-//                 (),
-//                 (),
-//                 (),
-//             )
-//             .await;
-//
-//         // result.unwrap();
-//         match result {
-//             Ok(value) => {
-//                 // Skip if no events found
-//                 if value.len() == 0 {
-//                     continue;
-//                 }
-//                 //
-//                 log(Type::Success, String::from("Got events"), &value);
-//                 // Process all events
-//                 for event in value {
-//                     // Unpack event arguments
-//                     let (from, to, value) = &event;
-//                     // Convert argument
-//                     let account_id = AccountId32::new(<[u8; 32]>::try_from(to.as_slice()).unwrap());
-//                     // Log arguments
-//                     log(Type::Success, String::from("Event"), &event);
-//                     log(Type::Info, String::from("From: "), from);
-//                     log(Type::Info, String::from("To: "), &account_id);
-//                     log(Type::Info, String::from("Value: "), value);
-//                 }
-//             }
-//             Err(error) => log(Type::Error, String::from("Shit happens"), &error)
-//         }
-//
-//         sleep(Duration::from_millis(2500)).await;
-//     }
-// }
-
 pub struct BSCAdapter<T: ContractEvents> {
     contract: Contract<WebSocket>,
     event_handler: T
