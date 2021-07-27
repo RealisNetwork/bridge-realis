13f59b7ce5c11ef0236c526380f3376f8e000b92
HolyGrease <kireevdanil2000@gmail.com>
2021-07-27 13:30:57 +0300

    refactor: remove unused staff

--- realis-sender/src/lib.rs
+++ realis-sender/src/lib.rs
@@ -27,38 +27,6 @@ use sp_core::crypto::SecretStringError;
 use logger::logger::{log, Type};
 use bsc_adapter::ContractEvents;
 
-// fn main() {
-//     env_logger::init();
-//     let url = String::from("rpc.realis.network");
-//
-//     // initialize api and set the signer (sender) that is used to sign the extrinsics
-//     let pair = Pair::from_string(&*from_path_to_account("res/accounts.key"), None).unwrap();
-//
-//     let api = Api::<sr25519::Pair>::new(format!("wss://{}", url)).map(|api| api.set_signer(pair)).unwrap();
-//
-//     // set the recipient
-//     let to: AccountId32 = AccountId32::from_str("1aa0d5c594a4581ec17069ec9631cd6225d5fb403fe4d85c8ec8aa51833fdf7f").unwrap();
-//
-//     // call Balances::transfer
-//     // the names are given as strings
-//     #[allow(clippy::redundant_clone)]
-//         let xt: UncheckedExtrinsicV4<_> = compose_extrinsic!(
-//         api.clone(),
-//         "Balances",
-//         "transfer",
-//         GenericAddress::Id(to),
-//         Compact(10_000 as u128)
-//     );
-//
-//     println!("[+] Composed Extrinsic:\n {:?}\n", xt);
-//
-//     // send and watch extrinsic until InBlock
-//     let tx_hash = api
-//         .send_extrinsic(xt.hex_encode(), XtStatus::InBlock)
-//         .unwrap();
-//     println!("[+] Transaction got included. Hash: {:?}", tx_hash);
-// }
-
 fn from_path_to_account<P: AsRef<Path>>(path: P) -> String {
     let string = fs::read_to_string(path).unwrap();
     return string
