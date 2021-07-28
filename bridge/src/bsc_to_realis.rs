75aec7d360de1cc09470cb0cddc78a097de72c25
Daelon02 <Daelon02@github.com>
2021-07-28 12:19:31 +0300

    Add nft, start fix clippy

--- bridge/src/bsc_to_realis.rs
+++ bridge/src/bsc_to_realis.rs
@@ -4,16 +4,18 @@ use realis_sender::RealisSender;
 pub async fn run() {
     let sender = RealisSender::new("rpc.realis.network");
 
-    let adapter = BSCAdapter::new(
+    // let adapter = BSCAdapter::new(
+    //     "wss://data-seed-prebsc-1-s1.binance.org:8545/",
+    //     sender.clone(),
+    // )
+    // .await;
+
+    let adapter_nft = BSCAdapter::new_nft(
         "wss://data-seed-prebsc-1-s1.binance.org:8545/",
-        sender.clone(),
+        sender,
     )
     .await;
 
-    // let adapter_nft =
-    //     BSCAdapter::new_nft("wss://data-seed-prebsc-1-s1.binance.org:8545/",
-    // sender)         .await;
-
-    // adapter_nft.listen_nft().await;
-    adapter.listen().await;
+    adapter_nft.listen_nft().await;
+    // adapter.listen().await;
 }
