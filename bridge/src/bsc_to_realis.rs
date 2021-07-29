2bd351d62e946fbf3e0df7db4c891992bce1d7c0
HolyGrease <kireevdanil2000@gmail.com>
2021-07-29 12:01:20 +0300

    feat: make listening bsc simultaneously

--- bridge/src/bsc_to_realis.rs
+++ bridge/src/bsc_to_realis.rs
@@ -1,20 +1,23 @@
 use bsc_adapter::BSCAdapter;
 use realis_sender::RealisSender;
+use futures::join;
 
 pub async fn run() {
     let sender = RealisSender::new("rpc.realis.network");
 
-    // let adapter = BSCAdapter::new(
-    //     "wss://data-seed-prebsc-1-s1.binance.org:8545/",
-    //     sender.clone(),
-    // )
-    // .await;
-    // adapter.listen().await;
+    let adapter = BSCAdapter::new(
+        "wss://data-seed-prebsc-1-s1.binance.org:8545/",
+        sender.clone(),
+    )
+    .await;
+    let token = adapter.listen();
 
     let adapter_nft = BSCAdapter::new_nft(
         "wss://data-seed-prebsc-1-s1.binance.org:8545/",
         sender,
     )
     .await;
-    adapter_nft.listen_nft().await;
+    let nft = adapter_nft.listen_nft();
+
+    join!(token, nft);
 }
