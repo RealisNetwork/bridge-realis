b1881229142020a2b600826da724c134775299bd
HolyGrease <kireevdanil2000@gmail.com>
2021-08-02 16:28:14 +0300

    init: first commit

---
+++ tests/src/accounts.rs
@@ -0,0 +1,116 @@
+pub mod realis {
+    use runtime::AccountId;
+    use sp_core::crypto::Ss58Codec;
+    use substrate_api_client::sp_runtime::app_crypto::{sr25519, Pair};
+
+
+    pub fn sudo() -> (AccountId, sr25519::Pair) {
+        let public =
+            AccountId::from_ss58check("5CSxbs1GPGgUZvsHNcFMyFRqu56jykBcBWBXhUBay2SXBsaA")
+                .unwrap();
+
+        let private: sr25519::Pair =
+            Pair::from_string(
+                "",
+                None
+            ).unwrap();
+
+        return (public, private);
+    }
+
+    pub fn alice() -> (AccountId, sr25519::Pair) {
+        let public =
+            AccountId::from_ss58check("5GKMzQnFtgEUUB8mLxXLvDkrj75AJJRaQs2XRTYgKgYQBdj1")
+                .unwrap();
+
+        let private: sr25519::Pair =
+            Pair::from_string(
+                "wish dynamic depth wait depart column farm abuse tail drink wear shallow",
+                None
+            ).unwrap();
+
+        return (public, private);
+    }
+
+    pub fn bob() -> (AccountId, sr25519::Pair) {
+        let public =
+            AccountId::from_ss58check("5F3uBTgjaUMWK7dZvTfiAjHw9y5teoUdCHfPziM7yoKMP5va")
+                .unwrap();
+
+        let private: sr25519::Pair =
+            Pair::from_string(
+                "they first film either access soft vanish boost finish hint identify bag",
+                None
+            ).unwrap();
+
+        return (public, private);
+    }
+
+    pub fn cindy() -> (AccountId, sr25519::Pair) {
+        let public =
+            AccountId::from_ss58check("5Grk9Ckfp6bunGUYzZXQEYabZkqa6WRkQYrQaKTPDxvAQ9JB")
+                .unwrap();
+
+        let private: sr25519::Pair =
+            Pair::from_string(
+                "industry trash cause horse kangaroo fiscal obey someone fortune shrimp wrestle fatigue",
+                None
+            ).unwrap();
+
+        return (public, private);
+    }
+}
+
+pub mod bsc {
+    use web3::types::Address;
+    use secp256k1::SecretKey;
+    use std::str::FromStr;
+
+    pub fn sudo() -> (Address, SecretKey) {
+        let public = Address::from_str("0x79abf92F6640B6D6540B116d4e7eA99ace932236")
+            .unwrap();
+
+        let private =
+            SecretKey::from_str(
+                ""
+            ).unwrap();
+
+        return (public, private);
+    }
+
+    pub fn alice() -> (Address, SecretKey) {
+        let public = Address::from_str("0xF09c06d1cDD0cF2B8Cc691197Eb1dd2d6889b5b1")
+            .unwrap();
+
+        let private =
+            SecretKey::from_str(
+                "f807147ea8a83e0951a23cbd2ff5c3a9cbaff5ca49234bd7b9d73b052b6fccb9"
+            ).unwrap();
+
+        return (public, private);
+    }
+
+    pub fn bob() -> (Address, SecretKey) {
+        let public = Address::from_str("0x75b882a65e82C00924a83e93bC39584C2Bb7a39B")
+            .unwrap();
+
+        let private =
+            SecretKey::from_str(
+                "2e7738fb3006d77faa35217a0f9fd7c03b19db0e717d4b909668b413d7b9b54f"
+            ).unwrap();
+
+        return (public, private);
+    }
+
+    pub fn cindy() -> (Address, SecretKey) {
+        let public = Address::from_str("0x6dFF4Fab42659d677c4c60F5FCC6993A96b5aaEa")
+            .unwrap();
+
+        let private =
+            SecretKey::from_str(
+                "f2d7a63c6aa4e22ade393093a5be78ec4f012c40d785b212aa2884e95f1b4b67"
+            ).unwrap();
+
+        return (public, private);
+    }
+}
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,7 +1,56 @@
+mod accounts;
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use self::super::accounts;
+    use runtime::AccountId;
+    use substrate_api_client::{Api, compose_extrinsic, XtStatus};
+    use substrate_api_client::sp_runtime::app_crypto::{sr25519, Pair};
+    use web3::types::Address;
+
+    fn api(signer: sr25519::Pair) -> Api<sr25519::Pair> {
+        let url = "rpc.realis.network";
+        // Create substrate api with signer
+        Api::<sr25519::Pair>::new(format!("wss://{}", String::from(url)))
+            .map(|api| api.set_signer(signer))
+            .unwrap()
+    }
+
+    fn give_tokens(to: AccountId, amount: u128) {
+        // Get sudo private key
+        let (_, private) = accounts::realis::sudo();
+        // Connect to Realis api
+        let api = api(private);
+        //
+        let xt = compose_extrinsic!(
+            api.clone(),
+            "Balances",
+            "transfer",
+            to,
+            amount
+        );
+
+        let tx_result =
+            api.send_extrinsic(xt.hex_encode(), XtStatus::InBlock);
+
+        match tx_result {
+            Ok(hash) => println!("Send extrinsic {:?}", hash),
+            Err(error) => println!("Can`t send extrinsic {:?}", error),
+        }
+    }
+
+    fn send_token_from_realis_to_bsc(signer: sr25519::Pair, from: AccountId, to: Address, amount: u128) {
+
+    }
+
+    #[tokio::test]
+    async fn simple_token_transfer() {
+        // Get Alice account
+        let (account_id, private) = accounts::realis::alice();
+        // Sudo give 1 token to A-realis
+        give_tokens(account_id, 1000);
+        // A-realis transfer 1 token to A-bsc
+        // send_token_from_realis_to_bsc(private, account_id, )
+        //
     }
 }
