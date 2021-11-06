ded1b1b7733faa70d1be91d9781771337af26332
HolyGrease <kireevdanil2000@gmail.com>
2021-11-06 16:35:29 +0200

    refactor: remove redundant staff

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -28,29 +28,3 @@ pub enum Error {
     #[error("{0}")]
     Custom(String),
 }
-
-pub enum Status {
-    Got = 0,
-    RealisError = 1,
-    RealisSuccess = 2,
-    BinanceError = 3,
-    BinanceSuccess = 4,
-    RollbackError = 5,
-    RollbackSuccess = 6,
-}
-
-impl Status {
-    #[must_use]
-    pub fn new(number: u32) -> Option<Self> {
-        match number {
-            0 => Some(Self::Got),
-            1 => Some(Self::RealisError),
-            2 => Some(Self::RealisSuccess),
-            3 => Some(Self::BinanceError),
-            4 => Some(Self::BinanceSuccess),
-            5 => Some(Self::RollbackError),
-            6 => Some(Self::RollbackSuccess),
-            _ => None,
-        }
-    }
-}
