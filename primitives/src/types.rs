23a812eee5eaf0863eedbe11f040c512c69cd0b0
HolyGrease <kireevdanil2000@gmail.com>
2021-11-08 15:09:40 +0200

    refactor: remove redundant staff

--- primitives/src/types.rs
+++ primitives/src/types.rs
@@ -1,16 +1,10 @@
-use sp_core::{H160, H256};
-pub use substrate_api_client::BlockNumber;
-use web3::types::{self, U64};
-
-pub type BscAccount = H160;
-pub type UserId = String;
-pub type Amount = u128;
-pub type Hash = H256;
+pub use substrate_api_client::{BlockNumber, Hash};
+use web3::types::{H256, U64};
 
 /// Undecoded bsc event
 #[derive(Debug, Clone)]
 pub struct RawEvent {
     pub block_number: Option<U64>,
-    pub hash: types::H256,
+    pub hash: H256,
     pub data: Vec<u8>,
 }
